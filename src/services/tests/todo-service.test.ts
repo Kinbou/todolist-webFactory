c01de1cc211861850338419a07c33c7428f2c274
Jennifer Bouquin <j.bouquin@atrnativ.com>
2023-09-17 17:48:19 +0200

    feat: :wrench: Change UnoCss, prettier & husky configRefactoring Dashboard, Pipeline pages & Update tests

--- src/services/tests/todo-service.test.ts
+++ src/services/tests/todo-service.test.ts
@@ -1,6 +1,7 @@
 import { describe, expect, expectTypeOf, test } from 'vitest'
 import { Todo } from '@models/Todo'
-import { getTodos } from '@/services/todo-service'
+import { getTodos, sortTodos } from '@/services/todo-service'
+import todos from '@services/data/todos.json'
 
 describe('getTodos Function', () => {
   test('returns data', () => {
@@ -11,5 +12,17 @@ describe('getTodos Function', () => {
   test('return type is Todo array', () => {
     const result = getTodos()
     expectTypeOf(result).toEqualTypeOf<Todo[]>()
+  })
+})
+
+describe('sortTodos Function', () => {
+  test('returns data', () => {
+    const result = sortTodos(todos[0], todos[1])
+    expect(result).not.toBeNull()
+  })
+
+  test('return type is number', () => {
+    const result = sortTodos(todos[0], todos[1])
+    expectTypeOf(result).toEqualTypeOf<number>()
   })
 })
